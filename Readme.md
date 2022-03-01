81b9f291fa1776d38f9ff279af7e271d5b3b9b23
alice-st <alice.stefanopoulou@gmail.com>
2022-03-01 20:02:27 +0200

    Make DARP sumbodule

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Optimization Results
+
+In essence, the DARP algorithm follows a cyclic coordinate descent optimization scheme updating each robots’ territory separately but towards achieving the overall mCPP objectives.
+
+<p align="center">
+  <img width="550" height="300" src="images/DARP.png">
+</p>
--- darp_x_optuna.py
+++ darp_x_optuna.py
@@ -1,83 +1,136 @@
-from objective_function import *
 import argparse
+import numpy as np
 import os
 import matplotlib as plt
-NUM_TRIALS = 50
+import optuna
+from optuna.samplers import TPESampler
+import sys
+sys.path.append('DARP')
+from multiRobotPathPlanner import MultiRobotPathPlanner
+from optuna.structs import TrialPruned
 
+NUM_TRIALS = 1000
 
-def optimize(rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, nep, portions, obstacles_positions, vis, num_drones):
+class optimize():
+    def __init__(self, rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, nep, portions, obstacles_positions, vis, num_drones):
+        # DARP Parameters
+        self.number_of_drones = num_drones
+        self.cols = cols
+        self.rows = rows
+        self.MaxIter = MaxIter
+        self.CCvariation = CCvariation
+        self.randomLevel = randomLevel
+        self.dcells = dcells
+        self.importance = importance
+        self.nep = nep
+        self.portions = portions
+        self.obstacles_positions = obstacles_positions
+        self.obstacles_coords = obstacles_positions
 
-    samplers = [optuna.integration.BoTorchSampler(),
-                optuna.samplers.TPESampler(),
-                optuna.samplers.CmaEsSampler()]
-    results = []
-    opt = Opt(rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, nep, portions, obstacles_positions, vis, num_drones)
+        # Optimization Parameters
+        self.samplers = [optuna.samplers.TPESampler()]
+        self.number_of_trials = 500
+        self.results = []
+        self.all_instances = []
 
-    for sampler in samplers:
-        study = optuna.create_study(
-            study_name="study",
-            directions=["maximize"],
-            sampler=sampler,
-            pruner=optuna.pruners.MedianPruner()
-        )
-        study.optimize(opt.objective, n_trials=NUM_TRIALS)
-        results.append(study)
+    def optimize(self):        
+        for sampler in self.samplers:
+            study = optuna.create_study(
+                study_name="study",
+                directions=["minimize"],
+                sampler=sampler,
+                pruner=optuna.pruners.MedianPruner()
+            )
+            study.optimize(self.objective, n_trials=self.number_of_trials)
+            self.results.append(study)
     
-    counter = 1
+    def objective(self, trial):
+        positions = []
 
-    for result in results:
-        print(f"Study{counter}")
-        print("Number of finished trials: ", len(result.trials))
+        for i in range(self.number_of_drones):
+            positions.append(trial.suggest_int(f"p{i}", 0, self.rows*self.cols-1))
+
+        if len(positions) != len(set(positions)):
+            self.all_instances.append("Pruned")
+            raise TrialPruned()
+
+        for obstacle in self.obstacles_positions:
+            for p in positions:
+                if p == obstacle:
+                    self.all_instances.append("Pruned")
+                    raise TrialPruned()
         
-        if obstacles_positions == []:
-            obs_flag = "No_obstacles"
+        multiRobotPathPlanner_instance = MultiRobotPathPlanner(self.rows, self.cols, self.nep, positions, self.portions, self.obstacles_coords, False)
+        self.all_instances.append(multiRobotPathPlanner_instance)
+        
+        if not multiRobotPathPlanner_instance.DARP_success:
+            raise TrialPruned()
         else:
-            obs_flag = "With_obstacles"
+            return max(multiRobotPathPlanner_instance.best_case.turns)
 
-        if not os.path.exists(f'x = {rows}, y = {cols}, num_drones = {num_drones}, obstacles = {obs_flag}'):
-            os.mkdir(f'x = {rows}, y = {cols}, num_drones = {num_drones}, obstacles = {obs_flag}')
 
-        for t in result.best_trials:
-            print(f"Best trial: {t.number}")
-            print(f'Best value: {t.values[0]}')
-            print(f'Best param: {t.params}')
-
-            with open(f'x = {rows}, y = {cols}, num_drones = {num_drones}, obstacles = {obs_flag}/{result.sampler}.txt', "a") as f1:
-                f1.write("Best trial: %s \nBest value: %s \nBest param: %s \n" % (t.number, t.values[0], t.params))
-                f1.close()
-
-        params = []
-        for param in result.trials[0].params.items():
-            params.append(param[0])
-
-        optuna.visualization.plot_contour(result, params=params)
-
-        temp = dict()
-        for i in range(len(result.trials)):
-            temp_all_results = list()
-            for index, value in result.trials[i].params.items():
-                temp_all_results.append(value)
+    def export_results(self):
+        counter = 1
+        for result in self.results:
+            print(f"Study{counter}")
+            print("Number of finished trials: ", len(result.trials))
             
-            temp_all_results = tuple(temp_all_results)
-            
-            if result.trials[i].values != None:
-                all_results[temp_all_results] = result.trials[i].values[0]
+            if self.obstacles_positions == []:
+                obs_flag = "No_obstacles"
             else:
-                all_results[temp_all_results] = None
+                obs_flag = "With_obstacles"
 
-        with open(f'x = {rows}, y = {cols}, num_drones = {num_drones}, obstacles = {obs_flag}/all_results.txt', "a") as f1:
-            f1.write(f'Sampler:{result.sampler}\n')
-            
-            for pos, res in all_results.items():
-                f1.write(f'{pos}, {res}\n')
-            
-            f1.close()
-        
-        fig = optuna.visualization.plot_contour(result, params=params)
-        fig.write_image(f'x = {rows}, y = {cols}, num_drones = {num_drones}, obstacles = {obs_flag}/{result.sampler}_contour.png')
+            if not os.path.exists(f'x = {self.rows}, y = {self.cols}, num_drones = {self.number_of_drones}, obstacles = {obs_flag}'):
+                os.mkdir(f'x = {self.rows}, y = {self.cols}, num_drones = {self.number_of_drones}, obstacles = {obs_flag}')
 
-        counter += 1
-   
+            best_avg = sys.maxsize
+            for t in result.best_trials:
+                print(f"Best trial: {t.number}")
+                print(f'Best value: {t.values[0]}')
+                print(f"Best Average: {self.all_instances[t.number].best_case.avg}")
+                print(f'Best param: {t.params}')
+                
+                if self.all_instances[t.number].best_case.avg < best_avg:
+                    best_avg = self.all_instances[t.number].best_case.avg
+                    self.best_trial = self.all_instances[t.number]
+
+
+                with open(f'x = {self.rows}, y = {self.cols}, num_drones = {self.number_of_drones}, obstacles = {obs_flag}/{result.sampler}.txt', "a") as f1:
+                    f1.write(f"Best trial: %s \nBest value: %s \nBest param: %s \nBest_avg: %s\n" % (t.number, t.values[0], t.params, self.all_instances[t.number].best_case.avg))
+                    f1.close()
+
+            params = []
+            for param in result.trials[0].params.items():
+                params.append(param[0])
+
+            optuna.visualization.plot_contour(result, params=params)
+
+            all_results = dict()
+            for i in range(len(result.trials)):
+                temp_all_results = list()
+                for index, value in result.trials[i].params.items():
+                    temp_all_results.append(value)
+                
+                temp_all_results = tuple(temp_all_results)
+                
+                if result.trials[i].values != None:
+                    all_results[temp_all_results] = result.trials[i].values[0]
+                else:
+                    all_results[temp_all_results] = None
+
+            with open(f'x = {self.rows}, y = {self.cols}, num_drones = {self.number_of_drones}, obstacles = {obs_flag}/all_results.txt', "a") as f1:
+                f1.write(f'Sampler:{result.sampler}\n')
+                
+                for pos, res in all_results.items():
+                    f1.write(f'{pos}, {res}\n')
+                
+                f1.close()
+            
+            fig = optuna.visualization.plot_contour(result, params=params)
+            fig.write_image(f'x = {rows}, y = {cols}, num_drones = {self.number_of_drones}, obstacles = {obs_flag}/{result.sampler}_contour.png')
+
+            counter += 1
+       
 
 if __name__ == '__main__':
 
@@ -154,5 +207,6 @@ if __name__ == '__main__':
     print("Robot Number:", num_drones)
     print("Portions for each Robot:", portions, "\n")
 
-    optimize(rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, args.nep, portions, args.obs_pos, args.vis, num_drones)
-
+    optimization = optimize(rows, cols, MaxIter, CCvariation, randomLevel, dcells, importance, args.nep, portions, args.obs_pos, args.vis, num_drones)
+    optimization.optimize()
+    optimization.export_results()
